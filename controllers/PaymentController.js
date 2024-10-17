dd0b81072b430cfe807ed248de5fd0aa27bfce7d
Waseem <wyameen152@gmail.com>
2024-10-17 16:28:49 +0500

    Added Email

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -3,13 +3,14 @@ const Stripe = require('stripe')
 const stripe = Stripe('sk_test_51QA221Rtm9jX1MDejT1gFPGLrWUnowHT3WDBAWbSUwZLuQ55tfkyDVLgWgVcZTYRmtj6ADHmn6nYATZPI4fMcxKW00YauqWE04');
 
 const ProcessPayment = async (req,res) => {
-  const { amount, currency } = req.body;
+  const { amount, currency, receipt_email } = req.body;
 
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
       currency: currency,
       payment_method_types: ["card"],
+      receipt_email: receipt_email,
     });
 
     res.send({
